cfb275f1adbf22e7f4f73716d890728a3a789e55
onecoolx <onecoolx@gmail.com>
2015-07-12 01:09:49 +0800

    fix javascriptcore generated files

--- source/webkit-2.2.8/Source/JavaScriptCore/generated_files.py
+++ source/webkit-2.2.8/Source/JavaScriptCore/generated_files.py
@@ -2,12 +2,38 @@
 
 import os
 import sys
+import subprocess
+import shutil
 
+# make dir
+def MakeDirIfNotExists(path):
+  is_exit = os.path.exists(path)
+  if not is_exit:
+    os.makedirs(path)
+
+# write file
+def WriteLinesToFile(p, path):
+  f = open(path, "a+")
+  contents = p.stdout.readlines()
+  for line in contents:
+    f.write(line)
+  f.close()
+
+# generate files
 def Main(src_dir, dst_dir):
+  # make output dir.
+  MakeDirIfNotExists(dst_dir)
+  # step 1, KeywordLookup.h 
+  subprocess.call(["python", src_dir+"/KeywordLookupGenerator.py", src_dir+"/parser/Keywords.table", ">", dst_dir+"/KeywordLookup.h"])
+  # step 2, RegExpJitTables.h 
+  subprocess.call(["python", src_dir+"/create_regex_tables", ">", dst_dir+"/RegExpJitTables.h"])
+  # step 3, Lexer.lut.h
+  p = subprocess.Popen("perl "+src_dir+"/create_hash_table "+src_dir+"/parser/Keywords.table", shell=True, stdout=subprocess.PIPE)
+  WriteLinesToFile(p, dst_dir+"/Lexer.lut.h")
+
   return 0
 
 
-
-
+# main
 if __name__ == '__main__':
   sys.exit(Main(sys.argv[1], sys.argv[2]))
