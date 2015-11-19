bfc4d742d1624ce0eb55d57504598601e6e140c1
godgodgod <onecoolx@gmail.com>
2015-11-19 14:11:17 +0800

    fix win32 build

--- source/webkit-2.2.8/Source/WebCore/generated_files.bat
+++ source/webkit-2.2.8/Source/WebCore/generated_files.bat
@@ -1,6 +1,7 @@
 
 set PATH=..\..\..\..\tools\win32\msys\bin;%PATH%.
-set GPERF=..\..\..\..\..\tools\win32\msys\bin\gperf.exe
+set PATH=..\..\..\..\..\tools\win32\msys\bin;%PATH%.
+set GPERF=gperf.exe
 
 set RUNPATH=%1
 %RUNPATH%\generated_files.py %1 "Generated"
