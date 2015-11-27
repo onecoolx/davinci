beb4d29aadf652a7f90cd5e45cd06b59ca16e773
godgodgod <onecoolx@gmail.com>
2015-11-27 18:02:42 +0800

    fix transform

--- source/WebKit/platform/graphics/AffineTransformDavinci.cpp
+++ source/WebKit/platform/graphics/AffineTransformDavinci.cpp
@@ -14,38 +14,38 @@
 namespace WebCore {
 
 AffineTransform::AffineTransform()
-	: m_matrix(ps_matrix_create())
+    : m_matrix(ps_matrix_create())
 {
     setMatrix(1, 0, 0, 1, 0, 0);
 }
 
 AffineTransform::AffineTransform(double a, double b, double c, double d, double e, double f)
-	: m_matrix(ps_matrix_create_init(a, b, c, d, e, f))
+    : m_matrix(ps_matrix_create_init(a, b, c, d, e, f))
 {
     setMatrix(a, b, c, d, e, f);
 }
 
 AffineTransform::~AffineTransform()
 {
-	ps_matrix_unref(m_matrix);
+    ps_matrix_unref(m_matrix);
 }
 
 AffineTransform::AffineTransform(const AffineTransform& other)
-	: m_matrix(ps_matrix_create_copy(other.m_matrix))
+    : m_matrix(ps_matrix_create_copy(other.m_matrix))
 {
     setMatrix(a(), b(), c(), d(), e(), f());
 }
 
 AffineTransform& AffineTransform::operator=(const AffineTransform& other)
 {
-	if (this == &other)
-		return *this;
+    if (this == &other)
+        return *this;
 
     setMatrix(other.m_transform);
 
-	ps_matrix_unref(m_matrix);
-	m_matrix = ps_matrix_create_copy(other.m_matrix);
-	return *this;
+    ps_matrix_unref(m_matrix);
+    m_matrix = ps_matrix_create_copy(other.m_matrix);
+    return *this;
 }
 
 PlatformMatrix AffineTransform::platformTransform(void) const
