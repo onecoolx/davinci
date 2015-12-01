2a6b0b2322caf12a038276e21fe79ae6cc1f9053
godgodgod <onecoolx@gmail.com>
2015-12-01 17:59:31 +0800

    add sound

---
+++ source/WebKit/platform/SoundDavinci.cpp
@@ -0,0 +1,17 @@
+/*
+ * SoundDavinci.cpp: Beep implements by davinci.
+ *
+ * Copyright(C) 2015 Zhang Ji Peng (onecoolx@gmail.com)
+ *
+ */
+
+#include "config.h"
+#include "Sound.h"
+
+namespace WebCore {
+
+void systemBeep()
+{
+}
+
+}
