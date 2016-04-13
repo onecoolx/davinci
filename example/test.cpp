b5a1960916549617b0da48147f7d099d0cacadf5
godgodgod <onecoolx@gmail.com>
2016-04-13 17:32:12 +0800

    fix test code

--- example/test.cpp
+++ example/test.cpp
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+#include <config.h>
+#include <wtf/ExportMacros.h>
+#include <wtf/MainThread.h>
+
+#include "Frame.h"
+#include "FrameView.h"
+#include "InitializeThreading.h"
+
+using namespace WebCore;
+
 int main(int, char*[])
 {
+    JSC::initializeThreading();
+    WTF::initializeMainThread();
+    //PlatformStrategiesWinCE::initialize();
+
+
+
     return 0;
 }
