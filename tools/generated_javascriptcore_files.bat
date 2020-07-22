61f1087ec8518030d208fe5ecfc44a6fd25e6aa1
onecoolx <onecoolx@gmail.com>
2020-07-21 21:08:46 -0700

    fix script

--- tools/generated_javascriptcore_files.bat
+++ tools/generated_javascriptcore_files.bat
@@ -2,4 +2,4 @@
 set SCRIPT_DIR=%~dp0
 set PATH=%SCRIPT_DIR%\win32\msys\bin;%PATH%.
 
-%SCRIPT_DIR%\generated_javascriptcore_files.py %1 %2
+python %SCRIPT_DIR%\generated_javascriptcore_files.py %1 %2
--- tools/generated_webcore_files.bat
+++ tools/generated_webcore_files.bat
@@ -3,4 +3,4 @@ set SCRIPT_DIR=%~dp0
 set PATH=%SCRIPT_DIR%\win32\msys\bin;%PATH%.
 set GPERF=gperf.exe
 
-%SCRIPT_DIR%\generated_webcore_files.py %1 "Generated"
+python %SCRIPT_DIR%\generated_webcore_files.py %1 "Generated"
