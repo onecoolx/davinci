433e9fb20ebb214c738eb6ea2847bb5e1c7906cc
zhangjipeng <zhangjipeng@xiaomi.com>
2024-08-14 18:18:58 +0800

    add webkit build part - 2 fix

--- third_party/webkit.cmake
+++ third_party/webkit.cmake
@@ -42,7 +42,7 @@ ExternalProject_Add(
   && ${CMAKE_COMMAND} -E copy
     "${PROJECT_ROOT}/packages/patchs/${WEBKIT_NAME}-${WEBKIT_VERSION}/OptionsDavinci.cmake"
     "${PROJECT_OUT}/${WEBKIT_NAME}/src/${WEBKIT_NAME}/Source/cmake/OptionsDavinci.cmake"
-  CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${PROJECT_OUT} -j4
+  CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${PROJECT_OUT} -j 4
   ${WEBKIT_OPTIONS}
 )
 
@@ -50,6 +50,7 @@ include_directories(${PROJECT_OUT}/include)
 link_directories(${PROJECT_OUT}/lib)
 
 add_dependencies(
+    ${WEBKIT_NAME}
     ${HARFBUZZ_NAME}
     ${ICU_NAME}
     ${FREETYPE_NAME}
